71407ab036ebd5e46828c05dc097155554328310
Roy Crivolotti <roycrivolotti@gmail.com>
2018-10-18 14:26:01 -0300

    Tidied up the js filed svgScroll.js after finishing it

--- scripts/svgScroll.js
+++ scripts/svgScroll.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     let svgPaths = document.querySelectorAll('#wrapper svg path');
-    console.log(svgPaths);
 
     document.addEventListener('scroll', () => {
-        let bounding = document.getElementById('wrapper').getBoundingClientRect();
-        if (bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
-            for (var path of svgPaths) {
-                path.classList.add('animationName');
-            }
-        }
+        let element = document.getElementById('wrapper');
+        if (isInViewPort(element)) svgPaths.forEach(path => path.classList.add('animationName'));
     });
+
+    let isInViewPort = element => {
+        let bounding = element.getBoundingClientRect();
+        return bounding.top >= 0 &&
+            bounding.left >= 0 &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth);
+    }
 });
