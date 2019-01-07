317d36da3c0c220c34805dd52c387e0f700aed4a
Roy Crivolotti <roycrivolotti@gmail.com>
2019-01-07 20:42:59 -0300

    Edited the top bar – might change it later or delete it altogether.

--- scripts/navscript.js
+++ scripts/navscript.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     // Disable the context menu for the nav menu --> it annoyed me on mobile...
     [...document.querySelectorAll('nav ul li a')].forEach(el => {
         el.addEventListener('contextmenu', event => event.preventDefault(), false);
@@ -11,31 +10,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let first = () => list.classList.toggle('hide');
     let second = () => main.classList.toggle('navactive');
+    let hidden_flag = true;
 
-
-    // Toggle the fade in/out – I also use a display: none on the unordered list, which I toggle here in order with a promise, just in case, as a double check to make sure it's not showing the list when it's supposed to be opaque/hidden
+    // Toggle the fade in/out
     function togglenav() {
         new Promise((resolve, reject) => {
                 first();
                 resolve(true);
             })
             .then(() => second());
-    }
 
-    function topbarColor(e) {
-        const topbar = document.querySelector('#topbar');
-        let bounding = document.getElementById('prog-languages').getBoundingClientRect();
+        hidden_flag = !hidden_flag;
 
-        if (bounding.top <= 30) {
-            topbar.querySelector('img').src = '/images/black-menu.svg';
-            topbar.querySelector('h1').style.color = 'black';
-        } else {
-            topbar.querySelector('h1').style.color = 'white';
-            topbar.querySelector('img').src = '/images/white-menu.svg';
-        }
+        document.querySelector('#topbar').classList.toggle('navactive');
+        document.querySelector('#topbar h1').classList.toggle('navactive');
+
+        if (!hidden_flag) document.querySelector('#topbar img').src = 'images/black-menu.svg'
+        else document.querySelector('#topbar img').src = 'images/white-menu.svg';
     }
 
     document.querySelector('#topbar img').addEventListener('click', togglenav);
     indexes.forEach(index => index.addEventListener('click', togglenav));
-    document.addEventListener('scroll', topbarColor);
 }, false);
