05f614be46e3ee663d7911cc3c35dc9bba4fd270
Roy Crivolotti <roycrivolotti@gmail.com>
2019-01-07 19:26:29 -0300

    Finished the nav – I'll test it live now.

--- scripts/navscript.js
+++ scripts/navscript.js
@@ -1,17 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Disable the context menu for the nav menu --> it annoyed me on mobile...
+    [...document.querySelectorAll('nav ul li a')].forEach(el => {
+        el.addEventListener('contextmenu', event => event.preventDefault(), false);
+    });
+
     const list = document.querySelector('nav ul'),
         main = document.querySelector('main'),
         indexes = [...document.querySelectorAll('nav ul li')];
-
-    console.log(indexes);
 
     let first = () => list.classList.toggle('hide');
     let second = () => setTimeout(() => {
         main.classList.toggle('navactive');
     }, 50);
 
-    function togglenav(event) {
+
+    // Toggle the fade in/out – I also use a display: none on the unordered list, which I toggle here in order with a promise, just in case, as a double check to make sure it's not showing the list when it's supposed to be opaque/hidden
+    function togglenav() {
         new Promise((resolve, reject) => {
                 first();
                 resolve(true);
@@ -20,4 +25,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     document.querySelector('#topbar img').addEventListener('click', togglenav);
+    indexes.forEach(index => index.addEventListener('click', togglenav));
 }, false);
