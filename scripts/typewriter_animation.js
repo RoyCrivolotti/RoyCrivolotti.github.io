c45c49232b15130adfc5dd93dc8ad0d32a4ef14b
Roy Crivolotti <roycrivolotti@gmail.com>
2019-03-03 14:21:42 -0300

    Updated my resume

--- scripts/typewriter_animation.js
+++ scripts/typewriter_animation.js
@@ -1,44 +1,45 @@
-    // each element in the array is to be printed in a new line
-    var text = new Array(
-        '<h3>Java, Javascript (vanilla), Node, MySQL, HTML & CSS </h3> <br>',
-        '<p>Si bien probé frameworks de frontend como Bootstrap 4 y Bulma, hasta el momento programé mayoritariamente sin frameworks</p>',
-        '<br><h3>¿En qué estoy ahora?</h3><br>',
-        '<p>Terminando el primer año de la UBA y la carrera intensiva de desarrollo web full stack de Acámica</p>',
-        '<p>Soy mentor en un <a href="https://www.coursera.org/learn/object-oriented-java" target="_blank">curso de OOP en Coursera </a></p>',
-        '<p>Apoyo mis estudios de ECMAScript 6 con los <a href="https://wesbos.com/courses/" target="_blank">cursos de Wes Bos</a>, con los cuales estoy por empezar a estudiar React y a profundizar lo que se de Node</p>',
-        '<p>En Java me interesa aprender Spring, pero por el momento mi experiencia es implementar algoritmos y estructuras de datos avanzadas</p>');
+// each element in the array is to be printed in a new line
+const text = [
+	'<h3>Java, C# .NET. Javascript (vanilla), Node, MySQL, HTML & CSS </h3> <br>',
+	'<p>Estoy disfrutando de mi primera experiencia laboral como desarrollador en Globant, aprendiendo C#.NET</p>',
+	'<br><h3>¿Qué más me mantiene ocupado ahora?</h3><br>',
+	'<p>Terminé el primer año de la UBA y la carrera intensiva de desarrollo web full stack de Acámica</p>',
+	'<p>Soy mentor en un <a href="https://www.coursera.org/learn/object-oriented-java" target="_blank">curso de OOP en Coursera </a></p>',
+	'<p>Apoyo mis estudios de ECMAScript 6 con los <a href="https://wesbos.com/courses/" target="_blank">cursos de Wes Bos</a>, con los cuales estoy empezando a estudiar React y a profundizar lo que se de Node</p>',
+	'<p>Siempre estoy interesado en aprender tecnologías nuevas y pulir lo que ya se</p>',
+];
 
-    var speed = 10; // time delay of print out
-    var index = 0; // start printing array at this posision
-    var arrLength = text[0].length; // the length of the text array
-    var scrollAt = 20; // start scrolling up at this many lines
-    var textPos = 0; // initialise text position
-    var contents = ''; // initialise contents variable
-    var row; // initialise current row
+const speed = 10; // time delay of print out
+let index = 0; // start printing array at this posision
+let arrLength = text[0].length; // the length of the text array
+const scrollAt = 20; // start scrolling up at this many lines
+let textPos = 0; // initialise text position
+let contents = ''; // initialise contents variable
+let row; // initialise current row
 
-    function typewriter() {
-        contents = ' ';
-        row = Math.max(0, index - scrollAt);
-        var destination = document.getElementById('animated_intro');
+function typewriter() {
+	contents = ' ';
+	row = Math.max(0, index - scrollAt);
+	const destination = document.getElementById('animated_intro');
 
-        while (row < index) {
-            contents += text[row++];
-        }
+	while (row < index) {
+		contents += text[row++];
+	}
 
-        destination.innerHTML = contents + text[index].substring(0, textPos) + '_';
-        if (textPos++ == arrLength) {
-            textPos = 0;
-            index++;
-            if (index != text.length) {
-                arrLength = text[index].length;
-                setTimeout('typewriter()', 500);
-            }
-        } else {
-            setTimeout('typewriter()', speed);
-        }
-    }
+	destination.innerHTML = `${contents + text[index].substring(0, textPos)}_`;
+	if (textPos++ == arrLength) {
+		textPos = 0;
+		index++;
+		if (index != text.length) {
+			arrLength = text[index].length;
+			setTimeout(typewriter, 500);
+		}
+	} else {
+		setTimeout(typewriter, speed);
+	}
+}
 
 
-    document.addEventListener('DOMContentLoaded', () => {
-        typewriter();
-    }, false);
+document.addEventListener('DOMContentLoaded', () => {
+	typewriter();
+}, false);
