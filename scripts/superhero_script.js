7f4f0047adf2a936ee548b112b4be66488be0b8a
Roy Crivolotti <roycrivolotti@gmail.com>
2019-04-10 11:53:13 -0300

    Run linter through last edit

--- scripts/superhero_script.js
+++ scripts/superhero_script.js
@@ -1,93 +1,91 @@
-/* eslint-disable */
-
 document.addEventListener('DOMContentLoaded', () => {
-    let previousOpenHero = document.querySelector('#hero-wrapper .superheros #batman');
-    let isEventAlreadyOn = false;
+	let previousOpenHero = document.querySelector('#hero-wrapper .superheros #batman');
+	let isEventAlreadyOn = false;
 
-    document.querySelector('.box').addEventListener('click', toggleOpen);
-    document.querySelectorAll('.superheros img').forEach(img => img.addEventListener('click', loadSuperHero));
+	document.querySelector('.box').addEventListener('click', toggleOpen);
+	document.querySelectorAll('.superheros img').forEach(img => img.addEventListener('click', loadSuperHero));
 
-    function toggleOpen(event) {
-        event.preventDefault();
-        event.stopPropagation();
+	function toggleOpen(event) {
+		event.preventDefault();
+		event.stopPropagation();
 
-        if (isEventAlreadyOn) return;
-        else isEventAlreadyOn = true;
+		if (isEventAlreadyOn) return;
+		isEventAlreadyOn = true;
 
-        let first = 'display';
-        const second = 'opening';
-        let third = 'open';
+		let first = 'display';
+		const second = 'opening';
+		let third = 'open';
 
-        const element = this.querySelector('#clickMe');
+		const element = this.querySelector('#clickMe');
 
-        if (this.classList.contains(first))[first, third] = [third, first];
-        else {
-            element.classList.toggle('hide');
-            setTimeout(() => { element.style.display = 'none'; }, 450);
-        }
+		if (this.classList.contains(first))[first, third] = [third, first];
+		else {
+			element.classList.toggle('hide');
+			setTimeout(() => { element.style.display = 'none'; }, 450);
+		}
 
-        this.classList.toggle(first);
+		this.classList.toggle(first);
 
-        new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    this.classList.toggle(second);
-                    resolve(true);
-                }, 500);
-            }).then(result => setTimeout(() => this.classList.toggle(third), 500))
-            .then(result => {
-                if (!this.classList.contains(first)) {
-                    element.style.display = 'inline';
-                    setTimeout(() => {
-                        element.classList.toggle('hide');
-                    }, 500);
-                }
+		new Promise((resolve, reject) => {
+			setTimeout(() => {
+				this.classList.toggle(second);
+				resolve(true);
+			}, 500);
+		}).then(result => setTimeout(() => this.classList.toggle(third), 500))
+			.then(result => {
+				if (!this.classList.contains(first)) {
+					element.style.display = 'inline';
+					setTimeout(() => {
+						element.classList.toggle('hide');
+					}, 500);
+				}
 
-                return true;
-            })
-            .then(() => isEventAlreadyOn = false);
-    }
+				return true;
+			})
+			.then(() => { isEventAlreadyOn = false; });
+	}
 
-    function loadSuperHero(event) {
-        event.preventDefault();
-        event.stopPropagation();
+	function loadSuperHero(event) {
+		event.preventDefault();
+		event.stopPropagation();
 
-        if (isEventAlreadyOn) return;
-        else isEventAlreadyOn = true;
+		if (isEventAlreadyOn) return;
+		isEventAlreadyOn = true;
 
-        const superheros = {
-            batman: 'I\'m<br><br>Batman',
-            flash: 'The<br><br>Flash',
-            wonder: 'Wonder<br><br><span style="margin-left: 60px";>Woman</span>',
-        };
+		const superheros = {
+			batman: 'I\'m<br><br>Batman',
+			flash: 'The<br><br>Flash',
+			wonder: 'Wonder<br><br><span style="margin-left: 60px";>Woman</span>',
+		};
 
-        if (previousOpenHero === this) {
-            isEventAlreadyOn = false;
-            document.querySelector('.box').click();
-            return;
-        }
-        else previousOpenHero = this;
+		if (previousOpenHero === this) {
+			isEventAlreadyOn = false;
+			document.querySelector('.box').click();
+			return;
+		}
+		previousOpenHero = this;
 
-        const box = document.querySelector('#hero-wrapper .box');
+		const box = document.querySelector('#hero-wrapper .box');
 
-        new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    box.classList.toggle('hide');
-                    resolve(true);
-                });
-            })
-            .then(result => {
-                setTimeout(() => {
-                    box.querySelector('h2').innerHTML = superheros[this.id];
+		new Promise((resolve, reject) => {
+			setTimeout(() => {
+				box.classList.toggle('hide');
+				resolve(true);
+			});
+		})
+			.then(result => {
+				setTimeout(() => {
+					box.querySelector('h2').innerHTML = superheros[this.id];
 
-                    Object.keys(superheros).map(e => document.querySelector('.box').classList.remove(e));
+					Object.keys(superheros).map(e => document.querySelector('.box').classList.remove(e));
 
-                    box.classList.toggle('hide');
-                    box.classList.toggle(this.id);
-                }, 580);
+					box.classList.toggle('hide');
+					box.classList.toggle(this.id);
+				}, 580);
 
 
-                return true;
-            })
-            .then(() => isEventAlreadyOn = false);
-    }
+				return true;
+			})
+			.then(() => { isEventAlreadyOn = false; });
+	}
 });
